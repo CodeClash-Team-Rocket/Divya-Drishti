837ee44015b66fc80117f4d9f97249e9f5610238
Romit  Dey <romit77dey@gmail.com>
2025-06-22 23:16:16 +0530

    showcase init

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import Hero from "./components/Hero";
 // import { HeroHighlight } from "@/components/ui/hero-highlight";
 // import { Spotlight } from "@/components/ui/spotlight-new";
+import Showcase from "./components/showcase";
 
 export default function page() {
   return (
     <div>
       <Hero />
+      <Showcase />
     </div>
   );
 }
