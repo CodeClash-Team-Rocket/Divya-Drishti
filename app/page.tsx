2158c4825a84489dd7ef38924fe884cc92090d54
Romit  Dey <romit77dey@gmail.com>
2025-06-25 11:50:17 +0530

    v1.1

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import Hero from "./components/Hero";
-// import { HeroHighlight } from "@/components/ui/hero-highlight";
-// import { Spotlight } from "@/components/ui/spotlight-new";
 import Showcase from "./components/showcase";
-import EmergencyButton from "./components/EmergencyButton";
+// import EmergencyButton from "./components/EmergencyButton";
 
 export default function page() {
   return (
     <div>
       <Hero />
       <Showcase />
-      {/* <EmergencyButton /> */}
     </div>
   );
 }
